cd4135842f6dc5392f71e876db5bbe0ad543708a
ahonn <ahonn95@outlook.com>
2019-02-03 14:52:26 +0800

    feat: add 35.search-insert-position.js

---
+++ algorithms/35.search-insert-position.js
@@ -0,0 +1,66 @@
+/*
+ * [35] Search Insert Position
+ *
+ * https://leetcode.com/problems/search-insert-position/description/
+ *
+ * algorithms
+ * Easy (40.35%)
+ * Total Accepted:    351.2K
+ * Total Submissions: 870.3K
+ * Testcase Example:  '[1,3,5,6]\n5'
+ *
+ * Given a sorted array and a target value, return the index if the target is
+ * found. If not, return the index where it would be if it were inserted in
+ * order.
+ *
+ * You may assume no duplicates in the array.
+ *
+ * Example 1:
+ *
+ *
+ * Input: [1,3,5,6], 5
+ * Output: 2
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: [1,3,5,6], 2
+ * Output: 1
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: [1,3,5,6], 7
+ * Output: 4
+ *
+ *
+ * Example 4:
+ *
+ *
+ * Input: [1,3,5,6], 0
+ * Output: 0
+ *
+ *
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const searchInsert = function searchInsert(nums, target) {
+  const len = nums.length;
+
+  let begin = 0;
+  let end = len - 1;
+  while (begin <= end) {
+    const mid = begin + ((end - begin) >> 1);
+    if (nums[mid] < target) {
+      begin = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+  return begin;
+};
