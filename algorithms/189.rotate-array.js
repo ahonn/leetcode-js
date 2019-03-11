1bcedfa07cf78a1b071af9c8647162c9b7f4aa32
ahonn <ahonn95@outlook.com>
2019-03-11 13:07:46 +0800

    feat: add 189.rotate-array.js

---
+++ algorithms/189.rotate-array.js
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode id=189 lang=javascript
+ *
+ * [189] Rotate Array
+ *
+ * https://leetcode.com/problems/rotate-array/description/
+ *
+ * algorithms
+ * Easy (29.07%)
+ * Total Accepted:    270.5K
+ * Total Submissions: 930.4K
+ * Testcase Example:  '[1,2,3,4,5,6,7]\n3'
+ *
+ * Given an array, rotate the array to the right by k steps, where k is
+ * non-negative.
+ *
+ * Example 1:
+ *
+ *
+ * Input: [1,2,3,4,5,6,7] and k = 3
+ * Output: [5,6,7,1,2,3,4]
+ * Explanation:
+ * rotate 1 steps to the right: [7,1,2,3,4,5,6]
+ * rotate 2 steps to the right: [6,7,1,2,3,4,5]
+ * rotate 3 steps to the right: [5,6,7,1,2,3,4]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: [-1,-100,3,99] and k = 2
+ * Output: [3,99,-1,-100]
+ * Explanation:
+ * rotate 1 steps to the right: [99,-1,-100,3]
+ * rotate 2 steps to the right: [3,99,-1,-100]
+ *
+ *
+ * Note:
+ *
+ *
+ * Try to come up as many solutions as you can, there are at least 3 different
+ * ways to solve this problem.
+ * Could you do it in-place with O(1) extra space?
+ *
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const rotate = function rotate(nums, k) {
+  const len = nums.length;
+  const pos = k % len;
+  const rotatePart = nums.splice(len - k);
+  nums.splice(0, 0, ...rotatePart);
+  return nums;
+};
