b68654cc801a958e1d594c52fb12ad9c79606443
ahonn <ahonn95@outlook.com>
2019-06-20 13:31:01 +0800

    feat: add 36.valid-sudoku.js

---
+++ algorithms/36.valid-sudoku.js
@@ -0,0 +1,112 @@
+/*
+ * @lc app=leetcode id=36 lang=javascript
+ *
+ * [36] Valid Sudoku
+ *
+ * https://leetcode.com/problems/valid-sudoku/description/
+ *
+ * algorithms
+ * Medium (42.13%)
+ * Total Accepted:    241.1K
+ * Total Submissions: 556.2K
+ * Testcase Example:  '[["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]'
+ *
+ * Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be
+ * validated according to the following rules:
+ *
+ * Each row must contain the digits 1-9 without repetition.
+ * Each column must contain the digits 1-9 without repetition.
+ * Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without
+ * repetition.
+ *
+ * A partially filled sudoku which is valid.
+ *
+ * The Sudoku board could be partially filled, where empty cells are filled
+ * with the character '.'.
+ *
+ * Example 1:
+ *
+ * Input:
+ * [
+ * ⁠ ["5","3",".",".","7",".",".",".","."],
+ * ⁠ ["6",".",".","1","9","5",".",".","."],
+ * ⁠ [".","9","8",".",".",".",".","6","."],
+ * ⁠ ["8",".",".",".","6",".",".",".","3"],
+ * ⁠ ["4",".",".","8",".","3",".",".","1"],
+ * ⁠ ["7",".",".",".","2",".",".",".","6"],
+ * ⁠ [".","6",".",".",".",".","2","8","."],
+ * ⁠ [".",".",".","4","1","9",".",".","5"],
+ * ⁠ [".",".",".",".","8",".",".","7","9"]
+ * ]
+ * Output: true
+ *
+ * Example 2:
+ *
+ * Input:
+ * [
+ * ["8","3",".",".","7",".",".",".","."],
+ * ["6",".",".","1","9","5",".",".","."],
+ * [".","9","8",".",".",".",".","6","."],
+ * ["8",".",".",".","6",".",".",".","3"],
+ * ["4",".",".","8",".","3",".",".","1"],
+ * ["7",".",".",".","2",".",".",".","6"],
+ * [".","6",".",".",".",".","2","8","."],
+ * [".",".",".","4","1","9",".",".","5"],
+ * [".",".",".",".","8",".",".","7","9"]
+ * ]
+ * Output: false
+ * Explanation: Same as Example 1, except with the 5 in the top left corner
+ * being
+ * ⁠   modified to 8. Since there are two 8's in the top left 3x3 sub-box, it
+ * is invalid.
+ *
+ * Note:
+ *
+ * A Sudoku board (partially filled) could be valid but is not necessarily
+ * solvable.
+ * Only the filled cells need to be validated according to the mentioned
+ * rules.
+ * The given board contain only digits 1-9 and the character '.'.
+ * The given board size is always 9x9.
+ */
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+const isValidSudoku = function isValidSudoku(board) {
+  const row = {};
+  const col = {};
+  const box = {};
+
+  for (let i = 0; i < board.length; i += 1) {
+    row[i] = new Uint8Array(10);
+    for (let j = 0; j < board[i].length; j += 1) {
+      if (col[j] === undefined) {
+        col[j] = new Uint8Array(10);
+      }
+      const cell = board[i][j];
+
+      if (cell !== '.') {
+        const x = Math.floor(i / 3);
+        const y = Math.floor(j / 3);
+
+        if (box[x] === undefined) {
+          box[x] = [
+            new Uint8Array(10),
+            new Uint8Array(10),
+            new Uint8Array(10),
+          ];
+        }
+
+        if (row[i][cell] || col[j][cell] || box[x][y][cell]) {
+          return false;
+        }
+        row[i][cell] = 1;
+        col[j][cell] = 1;
+        box[x][y][cell] = 1;
+      }
+    }
+  }
+
+  return true;
+};
