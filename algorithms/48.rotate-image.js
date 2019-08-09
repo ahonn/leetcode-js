e0d99bad0ada6bf91f13bacd477a246c2583c053
ahonn <ahonn95@outlook.com>
2019-08-09 14:02:05 +0800

    feat: add 48.rotate-image.js

---
+++ algorithms/48.rotate-image.js
@@ -0,0 +1,86 @@
+/*
+ * @lc app=leetcode id=48 lang=javascript
+ *
+ * [48] Rotate Image
+ *
+ * https://leetcode.com/problems/rotate-image/description/
+ *
+ * algorithms
+ * Medium (47.19%)
+ * Total Accepted:    274.3K
+ * Total Submissions: 550.2K
+ * Testcase Example:  '[[1,2,3],[4,5,6],[7,8,9]]'
+ *
+ * You are given an n x n 2D matrix representing an image.
+ *
+ * Rotate the image by 90 degrees (clockwise).
+ *
+ * Note:
+ *
+ * You have to rotate the image in-place, which means you have to modify the
+ * input 2D matrix directly. DO NOT allocate another 2D matrix and do the
+ * rotation.
+ *
+ * Example 1:
+ *
+ * Given input matrix =
+ * [
+ * ⁠ [1,2,3],
+ * ⁠ [4,5,6],
+ * ⁠ [7,8,9]
+ * ],
+ *
+ * rotate the input matrix in-place such that it becomes:
+ * [
+ * ⁠ [7,4,1],
+ * ⁠ [8,5,2],
+ * ⁠ [9,6,3]
+ * ]
+ *
+ * Example 2:
+ *
+ * Given input matrix =
+ * [
+ * ⁠ [ 5, 1, 9,11],
+ * ⁠ [ 2, 4, 8,10],
+ * ⁠ [13, 3, 6, 7],
+ * ⁠ [15,14,12,16]
+ * ],
+ *
+ * rotate the input matrix in-place such that it becomes:
+ * [
+ * ⁠ [15,13, 2, 5],
+ * ⁠ [14, 3, 4, 1],
+ * ⁠ [12, 6, 8, 9],
+ * ⁠ [16, 7,10,11]
+ * ]
+ */
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ *
+ * 1 2 3     7 8 9     7 4 1
+ * 4 5 6  => 4 5 6  => 8 5 2
+ * 7 8 9     1 2 3     9 6 3
+ * https://leetcode.com/problems/rotate-image/discuss/18872/A-common-method-to-rotate-the-image
+ */
+const rotate = function rotate(matrix) {
+  const len = matrix.length;
+  const halfLen = len >> 1;
+  for (let i = 0; i < halfLen; i += 1) {
+    const tempRow = matrix[i];
+    matrix[i] = matrix[len - i - 1];
+    matrix[len - i - 1] = tempRow;
+  }
+
+  for (let i = 0; i < len; i += 1) {
+    const row = matrix[i];
+    for (let j = i + 1; j < row.length; j += 1) {
+      const tempNum = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = tempNum;
+    }
+  }
+};
+
+console.log(rotate([[1, 2, 3], [4, 5, 6], [7, 8, 9]]));
