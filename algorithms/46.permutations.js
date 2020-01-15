0b646e445529c2d1abe5360f304e321c1b6eb9ff
ahonn <ahonn95@outlook.com>
2020-01-15 12:36:41 +0800

    feat: update 46.permutations.js

--- algorithms/46.permutations.js
+++ algorithms/46.permutations.js
@@ -32,21 +32,27 @@
  */
 const permute = function permute(nums) {
   const len = nums.length;
-  const ans = [];
+  const result = [];
+
+  const swap = (arr, i, j) => {
+    const tmp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = tmp;
+    return arr;
+  };
+
   const dfs = (numbers, start) => {
     if (start >= len) {
-      ans.push(numbers);
+      result.push(numbers);
+      return;
     }
-    for (let i = start; i < len; i += 1) {
-      const next = numbers.slice();
-      const swap = next[i];
-      next[i] = next[start];
-      next[start] = swap;
 
+    for (let i = start; i < len; i += 1) {
+      const next = swap(numbers.slice(), start, i);
       dfs(next, start + 1);
     }
   };
 
   dfs(nums, 0);
-  return ans;
+  return result;
 };
